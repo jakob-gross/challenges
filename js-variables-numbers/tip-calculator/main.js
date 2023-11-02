da6e068d6a9fa9d6d2ff863f7db4197640f1a9b2
Jakob Gross <jakobgross@gmail.com>
2023-11-02 17:18:06 +0100

    finished calculator

--- js-variables-numbers/tip-calculator/main.js
+++ js-variables-numbers/tip-calculator/main.js
@@ -1,1 +1,11 @@
 console.log("hi");
+let totalCost;
+let mealPrice;
+let tipPercentage;
+
+tipPercentage = 10;
+mealPrice = 15;
+
+tipAmount = mealPrice * (tipPercentage / 100);
+totalCost = tipAmount + mealPrice;
+console.log(`The total cost of your bill is: ${totalCost}`);

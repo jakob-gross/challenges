e2e5d4db9e689680a1f5572f2546ee2722d8989c
Jakob Gross <jakobgross@gmail.com>
2023-11-21 13:51:41 +0100

    finished all challenges

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,22 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const data = await fetch(url);
+    if (data.ok) {
+      const people = await data.json();
 
+      console.log(data);
+      console.log(people);
+      console.log(people.results[2].eye_color);
+    } else {
+      console.error("bad response");
+    }
+  } catch (error) {
+    console.error("an error occured");
+  }
+}
+try {
+} catch (error) {}
 fetchData();
--- js-fetch/js-fetch_star-wars/js/index.js
+++ js-fetch/js-fetch_star-wars/js/index.js
@@ -34,13 +34,20 @@ const EXAMPLE_DATA = {
 };
 
 // Create dom element for a card and append it to the root
-const firstCard = Card(EXAMPLE_DATA);
-renderElement(firstCard);
+// const firstCard = Card(EXAMPLE_DATA);
+// renderElement(firstCard);
 
-fetchDataAndRender();
+// fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch(`https://swapi.dev/api/people`);
+  const data = await response.json();
+  console.log(data);
+  data.results.forEach((card) => {
+    let newCard = Card(card);
+    renderElement(newCard);
+  });
 }
+fetchDataAndRender();

f6f4c95f107234ebbfc9996cf481ef43f7f67b63
Jakob Gross <jakobgross@gmail.com>
2023-11-28 10:28:47 +0100

    letzte challenges fast fertig

--- react-props/react-props_button/src/App.js
+++ react-props/react-props_button/src/App.js
@@ -2,5 +2,26 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <Button
+      colorB="red"
+      colorN="blue"
+      disabled
+      text="hallo hallo"
+      onClick={() => console.log("click")}
+    />
+  );
+}
+
+function Button({ colorB, colorN, disabled, text, onClick }) {
+  return (
+    <button
+      onClick={onclick}
+      disabled={disabled}
+      // disabled  ? "disabled" : null
+      style={{ backgroundColor: colorB, color: colorN }}
+    >
+      {text}
+    </button>
+  );
 }
